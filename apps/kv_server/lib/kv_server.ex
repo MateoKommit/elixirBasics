440b7771cb0f402a5301ebe70951decf3c9d93cd
Mateo Gregory Jimenez <MateoKommit>
2021-11-24 12:29:38 -0500

    Task and gen_tcp module finished
    Now server survives bucket crashes, and keep listening
    
    Also changed the implementation so now while the server tries to self restart
    a handshake is left to die

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -3,16 +3,42 @@ defmodule KVServer do
   Documentation for `KVServer`.
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
-
-      iex> KVServer.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp serve(socket) do
+    socket |> read_line() |> write_line(socket)
+    # write_line(read_line(socket), socket)
+    serve(socket)
+  end
+
+  @spec loop_acceptor(port | {:"$inet", atom, any}) :: no_return
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    serve_function = fn -> serve(client) end
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, serve_function)
+
+    :ok =  :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  @spec accept(char) :: no_return
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(
+      port,
+      [:binary, packet: :line, active: false, reuseaddr: true]
+    )
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
+
 end
